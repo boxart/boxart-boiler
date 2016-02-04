84b01bb63b4fff0712bdeacac379bec7bb65d31d
Corey Frang <gnarf@gnarf.net>
2016-02-04 14:55:52 -0500

    Add react-intl skeleton

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,19 @@
 import './styles/index.styl';
 
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import Main from './modules/main';
 
-ReactDom.render(
-  <Main />,
-  document.getElementById('root')
+import {addLocaleData, IntlProvider} from 'react-intl';
+import enLocaleData from 'react-intl/dist/locale-data/en';
+addLocaleData(enLocaleData);
+
+import strings from './locale/en/strings.json';
+
+ReactDOM.render(
+    <IntlProvider locale="en" messages={strings}>
+        <Main />
+    </IntlProvider>,
+    document.getElementById('root')
 );
